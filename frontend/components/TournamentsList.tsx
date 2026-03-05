12b97ac208eceaab11ebb863dc4568abfc0c325e
HugoANDRIAMAMPIANINA <hugo.andria15@gmail.com>
2026-03-05 16:55:42 +0100

    Fixed missing Tournament interface import

--- frontend/components/TournamentsList.tsx
+++ frontend/components/TournamentsList.tsx
@@ -1,16 +1,23 @@
+import { Tournmanent } from "@/common/interfaces/tournament.interface";
 import Link from "next/link";
-
 
 export default async function TournamentsList() {
   const response = await fetch("http://localhost:4321/tournaments");
   const data: Tournmanent[] = await response.json();
 
-  return <ul>
-    {data.map((tournament) =>
-      <li key={tournament.id}>
-        <p>{tournament.name}</p>
-        <Link className="bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none" href={`/tournaments/${tournament.id}`}>Voir plus</Link>
-      </li>
-    )}
-  </ul>;
+  return (
+    <ul>
+      {data.map((tournament) => (
+        <li key={tournament.id}>
+          <p>{tournament.name}</p>
+          <Link
+            className="bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none"
+            href={`/tournaments/${tournament.id}`}
+          >
+            Voir plus
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
