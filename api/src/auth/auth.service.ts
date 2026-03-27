d3b0e1a58ee5db6ca98fab3ad978809ffa18add6
HugoANDRIAMAMPIANINA <hugo.andria15@gmail.com>
2026-03-27 15:24:59 +0100

    Added RefreshToken logic for authentication and authorization; Added Navbar with user info; Updated User Avatar placeholder; Updated README.md

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,26 +1,80 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
+import { v4 as uuidv4 } from 'uuid';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from './entities/jwt-payload.interface';
+import { Request, Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+import { User } from 'src/users/entities/user.entity';
+import { Session } from './entities/session.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
+    private configService: ConfigService,
   ) {}
 
-  async signIn(
-    email: string,
-    password: string,
-  ): Promise<{ access_token: string }> {
+  async login(response: Response, email: string, password: string) {
     const user = await this.usersService.findOneByEmail(email);
     const passwordMatch = await bcrypt.compare(password, user.passwordHash);
     if (!passwordMatch) {
       throw new UnauthorizedException('Wrong password');
     }
 
+    const session = await this.generateSessionTokens(user);
+
+    const refreshTokenHash = await bcrypt.hash(session.refreshToken, 10);
+
+    await this.usersService.updateUserRefreshToken(user, refreshTokenHash);
+
+    this.setSessionCookies(response, session);
+  }
+
+  async logout(request: Request, response: Response) {
+    const user = await this.usersService.findOneByEmail(
+      request['user']['email'],
+    );
+    await this.usersService.updateUserRefreshToken(user, null);
+    this.clearSessionCookies(response);
+  }
+
+  async refresh(request: Request, response: Response) {
+    const accessToken: JwtPayload = this.jwtService.decode(
+      request['accessToken'],
+    );
+
+    const user = await this.usersService.findOneById(accessToken.sub);
+
+    const storedRefreshToken = user.refreshToken;
+    if (!storedRefreshToken) {
+      throw new UnauthorizedException('User has no refreshToken');
+    }
+
+    const refreshTokensEquals = await bcrypt.compare(
+      request['refreshToken'],
+      storedRefreshToken,
+    );
+
+    if (!refreshTokensEquals) {
+      throw new UnauthorizedException('RefreshTokens are different');
+    }
+
+    const session = await this.generateSessionTokens(user);
+
+    const updatedRefreshTokenHash = await bcrypt.hash(session.refreshToken, 10);
+
+    await this.usersService.updateUserRefreshToken(
+      user,
+      updatedRefreshTokenHash,
+    );
+
+    this.setSessionCookies(response, session);
+  }
+
+  private async generateSessionTokens(user: User): Promise<Session> {
     const payload: JwtPayload = {
       sub: user.id,
       email: user.email,
@@ -28,8 +82,37 @@ export class AuthService {
       organizedTournaments: user.organizedTournaments,
     };
 
-    return {
-      access_token: await this.jwtService.signAsync(payload),
+    const accessToken = await this.jwtService.signAsync(payload);
+    const refreshToken = uuidv4();
+
+    const session: Session = {
+      accessToken: accessToken,
+      refreshToken: refreshToken,
     };
+
+    return session;
+  }
+
+  private setSessionCookies(response: Response, session: Session) {
+    const secure = this.configService.get('NODE_ENV') === 'production';
+    response.cookie('accessToken', session.accessToken, {
+      httpOnly: true,
+      secure: secure,
+      sameSite: 'lax',
+      path: '/',
+      maxAge: this.configService.get<number>('TOKEN_COOKIES_EXPIRATION'),
+    });
+    response.cookie('refreshToken', session.refreshToken, {
+      httpOnly: true,
+      secure: secure,
+      sameSite: 'lax',
+      path: '/',
+      maxAge: this.configService.get<number>('TOKEN_COOKIES_EXPIRATION'),
+    });
+  }
+
+  private clearSessionCookies(response: Response) {
+    response.clearCookie('accessToken');
+    response.clearCookie('refreshToken');
   }
 }
--- api/src/main.ts
+++ api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    exposedHeaders: ['Set-Cookie'],
     credentials: true,
   });
 
@@ -23,6 +25,8 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+
+  app.use(cookieParser());
 
   await app.listen(process.env.API_PORT ?? 3000);
 }
